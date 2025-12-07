1d61e118366ca3b201b41643ea7f8aaff91e7d08
ramin rez <mohammadhossein.r423@gmail.com>
2025-12-07 12:56:54 +0330

    change icon route

--- src/Components/Projects/LinkSection.tsx
+++ src/Components/Projects/LinkSection.tsx
@@ -1,4 +1,5 @@
 import type { FC } from "react";
+import { Github, SquareArrowOutUpRight } from "lucide-react";
 
 type Props = {
   github: string;
@@ -9,10 +10,10 @@ const ProjectsCard: FC<Props> = ({ github, live }) => {
   return (
     <div className="flex gap-3 justify-self-end pb-2">
       <a href={github} target="_blank">
-        <img src="/public/github.png" alt="github" />
+        <Github color="gray" />
       </a>
       <a href={live} target="_blank">
-        <img src="/public/live.png" alt="demo" />
+        <SquareArrowOutUpRight color="gray" />
       </a>
     </div>
   );
