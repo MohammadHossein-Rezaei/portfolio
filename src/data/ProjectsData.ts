ef8320ca673900a39d55a5321a0e52740f89fe4d
ramin rez <mohammadhossein.r423@gmail.com>
2025-12-07 13:09:42 +0330

    change image route

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,7 +1,7 @@
 export const ProjectsData = [
   {
     id: "1",
-    image: "/public/habitat.png",
+    image: "habitat.png",
     title: "Habitat",
     description:
       "Habitat is a web application for managing goals and habits, built with React, TypeScript, and Tailwind CSS. Users can define various goals and set daily habits for each goal. The application also includes features for editing, deleting, tracking progress, and viewing statistical analysis of goals. The user interface is designed to be minimalistic, responsive, and user-friendly, with all user data stored locally using Local Storage.",
@@ -11,7 +11,7 @@ export const ProjectsData = [
   },
   {
     id: "2",
-    image: "/public/store.png",
+    image: "store.png",
     title: "ُStore",
     description:
       "Store is a modern, full-featured e-commerce web application built with React, TypeScript, and Tailwind CSS. The platform offers a seamless online shopping experience with product browsing, advanced filtering and shopping cart functionality .",
